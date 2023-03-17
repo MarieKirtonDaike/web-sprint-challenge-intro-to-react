0e593a719bb35c178543a4549845d5e3c1430e80
Emrie <emrie@macbook-pro.mynetworksettings.com>
2023-03-16 23:43:07 -0400

    finally got it to render the names

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+import Characters from './character';
+
 
 const App = () => {
+  const [character, setCharacters] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+useEffect(()=> {
+  axios.get('https://swapi.dev/api/people/')
+  .then(response => {
+    setCharacters(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}, []);
+
+ 
+const dataElements = character.map((item) => {
+  return (
+    <div key={item.id}>
+      <h2>{item.name}</h2>
+    </div>
+  );
+});
+  // const renderelements = character.map((char, index)=>{
+  //           return (<div key={index}> {char.name}</div>)})
+    
+  
+
+ 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
+      
       <h1 className="Header">Characters</h1>
+     <div key={dataElements.id}>{dataElements}</div>
+      {/* <Characters character={character} /> */}
     </div>
   );
 }
---
+++ src/character.js
@@ -0,0 +1,30 @@
+// import React from "react";
+// import styled from 'styled-components'
+
+// const Characters=({character})=>{
+    
+//     const Wrapperdiv = styled.div
+//     ` 
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     background-color: white;
+//     border: 1px solid black;
+//     margin: 20px 100px;
+//     padding: 70px; 
+//     `
+//     const renderelements = character.map((char, index)=>{
+//         return (<Wrapperdiv key={index}> {char.name}</Wrapperdiv>)})
+
+
+//  return(
+//     <div>{renderelements}</div>
+//  )
+
+
+// }
+
+
+
+
+// export default Characters
